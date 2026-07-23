1cdf4e0b06e22d41a408a2d3831f47cfd8a907ec
nelsonoche5-commits <nelsonoche5@gmail.com>
2026-07-23 12:07:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+const supabaseUrl = "https://avprmuydtwcnvbqzvtct.supabase.co";
+const supabaseKey = "sb_publishable_2Xvf3bbcfZC-YkRptjeavg_STPeXWTH";
 
 const supabaseClient = supabase.createClient(
   supabaseUrl,
@@ -7,20 +7,19 @@ const supabaseClient = supabase.createClient(
 );
 
 document.getElementById("signupBtn").addEventListener("click", async () => {
-
-  const email = document.getElementById("email").value;
+  const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value;
   const message = document.getElementById("message");
 
   const { data, error } = await supabaseClient.auth.signUp({
-    email: email,
-    password: password
+    email,
+    password
   });
 
   if (error) {
     message.innerText = error.message;
   } else {
-    message.innerText = "Account created successfully!";
+    message.innerText =
+      "Account created successfully! Check your email to verify your account.";
   }
-
 });
